ed304945132e04422c7ffa12f0bb452fb42ad77d
callaito <radelinrsm029@gmail.com>
2024-03-08 08:25:58 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Double numero;
+
+Console.Write("numero: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+if (numero > 0)
+
+{
+    Console.WriteLine("El numero es positivo");
+}
+
+else if (numero == 0)
+{
+    Console.WriteLine("El numero es neutro");
+}
+
+else
+{
+    Console.WriteLine("El numero es negativo");
+}
